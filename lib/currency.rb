04f8c7314f2919231bf127ce6470db11dfce1650
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-17 15:24:35 -0500

    able to add to another Currency object with the same currency code

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,7 +1,5 @@
 
 
-# Should NOT equal another Currency object with different amount or currency code
-# Should be able to be added to another Currency object with the same currency code
 # Should be able to be subtracted by another Currency object with the same currency code
 # Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
 # Should be able to be multiplied by a Fixnum or Float and return a new Currency object
@@ -16,6 +14,13 @@ class Currency
   def ==(other)
     if self.currency_code == other.currency_code
       self.amount == other.amount
+    end
+  end
+
+  def +(other)
+    if self.currency_code == other.currency_code
+      code = self.currency_code
+      Currency.new((self.amount + other.amount), code)
     end
   end
 end
--- spec/currency_test.rb
+++ spec/currency_test.rb
@@ -6,37 +6,53 @@ class CurrencyTest < Minitest::Test
   def setup
   end
 
+  def c(amount, code)
+    Currency.new(amount, code)
+  end
+
   def test_exists?
-    currency = Currency.new(5, nil)
+    currency = c(5, nil)
     assert(currency)
   end
 
   def test_amount
-    currency = Currency.new(5, nil)
+    currency = c(5, nil)
     assert_equal(currency.amount, 5)
   end
 
   def test_currency_code
-    currency = Currency.new(nil, 'USD')
+    currency = c(nil, 'USD')
     assert_equal(currency.currency_code, 'USD')
   end
 
   def test_currency_equality
-    assert_equal(Currency.new(10, 'USD'), Currency.new(10, 'USD'))
-    assert_equal(Currency.new(20, 'USD'), Currency.new(20, 'USD'))
-    assert_equal(Currency.new(125, 'JPY'), Currency.new(125, 'JPY'))
+    assert_equal(c(10, 'USD'), c(10, 'USD'))
+    assert_equal(c(20, 'USD'), c(20, 'USD'))
+    assert_equal(c(125, 'JPY'), c(125, 'JPY'))
   end
 
   def test_currency_must_be_the_same_type
-    refute_equal(Currency.new(10, 'USD'), Currency.new(10, 'JPY'))
-    refute_equal(Currency.new(20, 'USD'), Currency.new(20, 'JPY'))
-    refute_equal(Currency.new(125, 'JPY'), Currency.new(125, 'USD'))
+    refute_equal(c(10, 'USD'), c(10, 'JPY'))
+    refute_equal(c(20, 'USD'), c(20, 'JPY'))
+    refute_equal(c(125, 'JPY'), c(125, 'USD'))
   end
 
   def test_currency_is_not_equal_if_ammount_is_different
-    refute_equal(Currency.new(10, 'USD'), Currency.new(11, 'USD'))
-    refute_equal(Currency.new(20, 'USD'), Currency.new(21, 'USD'))
-    refute_equal(Currency.new(125, 'JPY'), Currency.new(126, 'JPY'))
+    refute_equal(c(10, 'USD'), c(11, 'USD'))
+    refute_equal(c(20, 'USD'), c(21, 'USD'))
+    refute_equal(c(125, 'JPY'), c(126, 'JPY'))
+  end
+
+  def test_adding_currencies_together
+    assert_equal(c(20, 'USD'), (c(10, 'USD') + c(10, 'USD')))
+    assert_equal(c(40, 'USD'), (c(20, 'USD') + c(20, 'USD')))
+    assert_equal(c(250, 'JPY'), (c(125, 'JPY')+ c(125, 'JPY')))
+  end
+
+  def test_currencies_without_the_same_code_cannot_be_added
+    assert_equal(nil, (c(10, 'JPY') + c(10, 'USD')))
+    assert_equal(nil, (c(20, 'JPY') + c(20, 'USD')))
+    assert_equal(nil, (c(125, 'USD')+ c(125, 'JPY')))
   end
 
 end
